e6b5f764eb6beea732d5568d0bdb5542b2ff99ed
Famous_person <93986253+famous-sodin@users.noreply.github.com>
2025-06-09 07:17:38 +0000

    Pending changes exported from your codespace

---
+++ test.py
@@ -0,0 +1,7 @@
+number = input("원하는 숫자를 ,로 구분하여 입력 : ").split(",")
+total=[]
+number=number.sort()
+for i in range(len(number)):
+    if int(number[i-1])%2==0:
+        total.append(int(number[i-1]))
+print(f'짝수는 {total}')
